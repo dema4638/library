323776f29c949a09a081078afbd48bfc6861249e
dema4638 <deimenamar@gmail.com>
2020-03-02 22:02:49 +0200

    initial commit 2

--- src/main/java/SimpleHTTPServer.java
+++ src/main/java/SimpleHTTPServer.java
@@ -1,5 +1,6 @@
 import com.sun.net.httpserver.HttpServer;
 
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -8,7 +9,7 @@ public class SimpleHTTPServer {
     public static void main(String[] args) throws Exception{
         Library.initLibrary();
         ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
-        HttpServer server = HttpServer.create(new InetSocketAddress("192.168.1.101", 80),10);
+        HttpServer server = HttpServer.create(new InetSocketAddress(InetAddress.getLocalHost(), 80),10);
         server.createContext("/Library", new  LibraryHttpHandler());
         server.setExecutor(threadPoolExecutor);
         server.start();
